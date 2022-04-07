64d6816456986e6756eaea82dce78a53fc42cb5b
Nurivan Gomez <nurivan.gomez@gmail.com>
2022-04-07 14:29:04 -0500

    Basic ranking algorithm

--- start.js
+++ start.js
@@ -19,8 +19,50 @@ async function getPlayers(apiKey, clanTag) {
 // TODO - Dehardcode clan tag --- Will it come from ui or from a file?
 async function main() {
     const apiKey = fs.readFileSync("./token.txt").toString();
-    let res = await getPlayers(apiKey, "%239YQQQ98");
-    console.log(res);
+
+    let playerScores = new Map();
+    getPlayers(apiKey, "%239YQQQ98")
+        .then(players => {
+            //Default sorted by trophies
+            for(let i = 0; i < players.length; i++) {
+                let player = players[i];
+                playerScores.set(player.tag, {name: player.name, score: i});
+            }
+
+            //Sorting by donations
+            players = players.sort((a, b) => {
+                return b.donations - a.donations;
+            });
+            for(let i = 0; i < players.length; i++) {
+                let player = players[i];
+                // console.log(player.donations);
+                playerScores.get(player.tag).score += i;
+            }
+
+            //Sorting by lastSeen
+            players = players.sort((a, b) => {
+                if(a.lastSeen >= b.lastSeen) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            });
+            for(let i = 0; i < players.length; i++) {
+                let player = players[i];
+                playerScores.get(player.tag).score += i;
+            }
+
+            //Sorting final rankings
+            let finalRanks = [...playerScores.entries()].sort((a, b) => {
+                return a[1].score - b[1].score;
+            });
+            for(let player of finalRanks) {
+                console.log(player);
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
 }
 
 if (require.main === module) {
